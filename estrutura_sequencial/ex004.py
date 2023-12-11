1c9f494dbbf3f536311a86475ae2f1ca5f16f027
FranciscoLima <francisco1111.fl@protonmail.com>
2023-12-11 09:00:49 -0300

    ex004 - grade average

---
+++ estrutura_sequencial/ex004.py
@@ -0,0 +1,9 @@
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+grade1 = float(input())
+grade2 = float(input())
+grade3 = float(input())
+grade4 = float(input())
+
+average = (grade1 + grade2 + grade3 + grade4) / 4
+
+print(average)
