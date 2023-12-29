9765406bd5555568af44cfc389fe73571c799dd3
LimaFrancisco <francisco1111.fl@protonmail.com>
2023-12-29 11:48:56 -0300

    ex022 - even or odd

---
+++ 02_estrutura_de_decisao/ex022.py
@@ -0,0 +1,8 @@
+"""Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica: utilize o operador módulo (resto da divisão)."""
+
+num = int(input("enter a interger: "))
+
+if num % 2 == 0:
+    print("even number")
+else:
+    print("odd number")
