3c011c04e0a905ac9ac67d54173ec17b9db82ecb
LimaFrancisco <francisco1111.fl@protonmail.com>
2024-01-02 09:54:39 -0300

    ex004 - how many years until country a reaches country b

---
+++ 03_estrutura_de_repeticao/ex004.py
@@ -0,0 +1,14 @@
+"""Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3% e que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%. Faça um programa que calcule e escreva o número de anos necessários para que a população do país A ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento."""
+
+countryA = 80000
+countryB = 200000
+
+years = 0 
+
+while countryA < countryB:
+    countryA += countryA * 0.03
+    countryB += countryB * 0.015
+
+    years += 1
+
+print(f"{years}")
