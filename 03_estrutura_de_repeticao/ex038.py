5313ab84e02563f02cade36c56d5b5b18327d08c
LimaFrancisco <francisco1111.fl@protonmail.com>
2024-01-17 12:23:47 -0300

    ex038 - calculating current salary with user input

--- 03_estrutura_de_repeticao/ex038.py
+++ 03_estrutura_de_repeticao/ex038.py
@@ -4,7 +4,9 @@
     Em 1996 recebeu aumento de 1,5% sobre seu salário inicial;
     A partir de 1997 (inclusive), os aumentos salariais sempre correspondem ao dobro do percentual do ano anterior. Faça um programa que determine o salário atual desse funcionário. Após concluir isto, altere o programa permitindo que o usuário digite o salário inicial do funcionário. """
 
-salary = 1000 + (1000 * 0.015) 
+salary = float(input("insert your salary: "))
+
+salary += (salary * 0.015) 
 
 increase = 0.015
 
