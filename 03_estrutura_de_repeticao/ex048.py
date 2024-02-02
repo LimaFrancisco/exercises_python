d20ccfda40f735b35ff3c5d7ed5ca713221dc8f5
LimaFrancisco <francisco1111.fl@protonmail.com>
2024-02-02 10:34:58 -0300

    ex048 - inverted integer

---
+++ 03_estrutura_de_repeticao/ex048.py
@@ -0,0 +1,11 @@
+"""Faça um programa que peça um numero inteiro positivo e em seguida mostre este numero invertido.
+
+    Exemplo: 
+    12376489
+  => 98467321
+    """
+
+num = input("enter a interger numeber: ")
+num_inverted = num[::-1]
+
+print(f"=> {num_inverted}")
