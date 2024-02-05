c41e599b6d022e0e65b31f7d19b1f70b35d0e73d
LimaFrancisco <francisco1111.fl@protonmail.com>
2024-02-05 09:01:20 -0300

    ex001 - putting five values into a list

---
+++ 04_execicios_listas/ex001.py
@@ -0,0 +1,8 @@
+"""Faça um Programa que leia um vetor de 5 números inteiros e mostre-os. """
+
+vector = []
+
+for index in range(0,5):
+    vector.append(int(input("enter a integer number: ")))
+
+print(vector)
