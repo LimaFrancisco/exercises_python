fcfb87dba31f96892eb6909dd785361543fe5cb0
LimaFrancisco <francisco1111.fl@protonmail.com>
2024-02-22 09:59:43 -0300

    ex003 - name vertically

---
+++ 06-exercicios_com_strings/ex003.py
@@ -0,0 +1,13 @@
+"""Nome na vertical. Faça um programa que solicite o nome do usuário e imprima-o na vertical.
+F
+U
+L
+A
+N
+O
+"""
+
+name = input("enter you name: ")
+
+for item in name:
+    print(item)
