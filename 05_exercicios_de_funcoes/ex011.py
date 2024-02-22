c2482597b9fe6a3390dbdab7cff96a533440ce23
LimaFrancisco <francisco1111.fl@protonmail.com>
2024-02-21 21:26:25 -0300

    ex011 - function to write a date in full

--- 05_exercicios_de_funcoes/ex011.py
+++ 05_exercicios_de_funcoes/ex011.py
@@ -1,6 +1,16 @@
 """Data com mês por extenso. Construa uma função que receba uma data no formato DD/MM/AAAA e devolva uma string no formato D de mesPorExtenso de AAAA. Opcionalmente, valide a data e retorne NULL caso a data seja inválida. """
 
-def date_in_full(date):
-    day = ("1st","2nd","3rd","4th","5th","6th","7th","8th","9th","10th","11th","12th","13th","14th","15th","16th","17th","18th","19th","20th","21st","22nd","23th","24th","25th","26th","27th","28th","29th","30th","31st")
+from datetime import datetime
 
-    month = "januray","february","march","april","may","juny","july","august","september","october","november","december"
+def date_in_full(date):
+    try:
+        valid_date = datetime.strptime(date, '%d/%m/%Y')
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 
+                  'July', 'August', 'September', 'October', 'November', 'December']
+        month_in_full = months[valid_date.month - 1]
+        return f"{valid_date.day} of {month_in_full} of {valid_date.year}"
+    except ValueError:
+        return None
+
+# Test the function
+print(date_in_full(input("enter a valid date: "))) 
