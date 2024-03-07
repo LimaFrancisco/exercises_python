b335a1ad68274f51cabf6a4c0ccf5007bd52e529
LimaFrancisco <francisco1111.fl@protonmail.com>
2024-03-07 09:04:53 -0300

    ex003 - amount of floor and baseboard for a local

--- 08-exercicios_classes/ex003/app.py
+++ 08-exercicios_classes/ex003/app.py
@@ -6,11 +6,11 @@ base = float(input("what is the size of the base? "))
 height = float(input("what is the size of the height? "))
 
 local = rectangle(base, height)
-
 ceramicFloor = rectangle(0.56, 0.32) # most common sizes
 
 addition = ceramicFloor.area() * 0.1
-
 final_calculation = ceil((local.area() / ceramicFloor.area()) * addition) + (local.area() / ceramicFloor.area())
 
-print(f"\nyou need to pay {ceil(final_calculation):.0f} ceramics for your floor")
+baseboard = local.perimeter() + (local.perimeter() * 0.1)
+
+print(f"\nyou need to buy {ceil(final_calculation):.0f} ceramics and {baseboard:.2f} meters of baseboard for your floor")
