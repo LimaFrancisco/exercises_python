582da81cc063370a2587cf7b4f59bff588455000
LimaFrancisco <francisco1111.fl@protonmail.com>
2024-03-08 09:06:29 -0300

    ex006 - creating class tv

---
+++ 08-exercicios_classes/ex006/app.py
@@ -0,0 +1,16 @@
+from tv import tv
+
+myTv = tv()
+
+myTv.volume_up()
+myTv.volume_up()
+myTv.volume_up()
+myTv.volume_up()
+
+myTv.change_channel(25)
+myTv.change_channel(180)
+myTv.change_channel(-12)
+
+myTv.volume_up()
+myTv.volume_up()
+myTv.volume_up()
