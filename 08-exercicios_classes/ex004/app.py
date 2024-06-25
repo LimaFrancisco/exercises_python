7ae2d207e2f084cb5d1529d29cccacef395e829c
Francisco Lima <francisco1111.fl@protonmail.com>
2024-06-25 15:36:20 -0300

    Delete 08-exercicios_classes directory
    repository deleted

--- 08-exercicios_classes/ex004/app.py
+++
@@ -1,7 +0,0 @@
-from person import person
-
-man = person("Fernando", 25, 85, 1.80)
-woman = person("Sasha", 20, 61, 1.60)
-
-man.to_lose_weight()
-woman.getting_older()
--- 08-exercicios_classes/ex005/app.py
+++
@@ -1,7 +0,0 @@
-from current_acount import current_acount
-
-acount = current_acount(666, "ozzy osbourne")
-
-acount.cash_deposit(800)
-
-acount.change_nome("jack white")
